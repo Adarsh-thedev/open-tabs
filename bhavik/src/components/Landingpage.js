e4cdc80cf3d93e8490a217ca6755ba6ea754d92b
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-05-17 13:37:15 -0400

    Update Landingpage.js

--- bhavik/src/components/Landingpage.js
+++ bhavik/src/components/Landingpage.js
@@ -4,8 +4,6 @@ import Modal from 'react-modal';
 import './styles.css';
 import Header from './Header';
 import Searchbar from './Searchbar';
-import MultiStepForm from './MultiStepForm';
-import $ from 'jquery';
 
 const NAME_LS = 'NAME_LS';
 const EMAIL_LS = 'EMAIL_LS';
@@ -74,88 +72,6 @@ export default class Landingpage extends Component {
       }
     
       componentDidMount() {
-        //jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
-
-$(".next").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
-
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
-
-$(".submit").click(function(){
-	return false;
-})
-
         const name = localStorage.getItem(NAME_LS);
         if (name) {
           this.setState({name});
@@ -213,9 +129,7 @@ $(".submit").click(function(){
                   style={customStyles}
                   contentLabel="name-modal"
                 >
-
-                  {/* <MultiStepForm /> */}
-                  {/* <div className="modal-content">
+		<div className="modal-content">
                     <div className="modal-title">What's your name?</div>
                       <div className="modal-input" >
                         <form>
@@ -235,33 +149,7 @@ $(".submit").click(function(){
                           Enter
                         </button>
                       </div>
-                  </div> */}
-
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <form id="msform">
-            <fieldset>
-           
-                <input type="text" name="fname" placeholder="Name"/>
-                <input type="button" name="next" class="next action-button" value="Next"/>
-            </fieldset>
-          
-            <fieldset>
-                <input type="email" name="Email" placeholder="Email"/>
-                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                <input type="button" name="next" class="next action-button" value="Next"/>
-            </fieldset>
-            <fieldset>
-                <input type="password" name="pass" placeholder="Password"/>
-                <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                <input type="submit" name="submit" class="submit action-button" value="Submit"/>
-            </fieldset>
-        </form>
-    </div>
-</div>
-
-
-
+                  </div>
                 </Modal>
               </div>
               <div className="Search">
