37834119818312d5aabd1e4566995f1752d18b82
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-05-20 13:39:45 -0400

    Add files via upload

--- bhavik/src/components/Header.js
+++ bhavik/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from 'react';
 import logo from '../assets/logo.png';
 import './styles.css';
-import {FiSettings, FiHome, FiUser, FiBell} from 'react-icons/fi';
+import {FiSettings, FiHome, FiUser, FiBell, FiGift} from 'react-icons/fi';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { Row, Col } from 'react-bootstrap';
 import Tabcounter from './Tabcounter';
@@ -14,19 +14,30 @@ function Update() {
   const [show, setShow] = useState(true);
 
   return (
-    <Row>
+    <Row style={{
+      position: 'relative',
+      margin: '0 -15px'
+    }}>
       <Col xs={9}>
-        <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+        <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide style={{
+      position: 'absolute'
+    }}> 
           <Toast.Header>
-            <img
-              src="holder.js/20x20?text=%20"
-              className="rounded mr-2"
-              alt=""
-            />
-            <strong className="mr-auto">Update (May 2020)</strong>
-            <small>11 mins ago</small>
+            <strong className="mr-auto" style={{textAlign: 'left'}}>OpenTabs' Brand New Look!<br />(Update May 2020)</strong>
+            {/* <small>11 mins ago</small> */}
           </Toast.Header>
-          <Toast.Body>Welcome to your new extension! Now Better!</Toast.Body>
+          <Toast.Body style={{textAlign: 'justify'}}>
+          After much preparation and hard work, we are launching our ready-for-market version of OpenTabs. A huge thank you to our 100 beta users and their faith in us for helping us reach this milestone. We have made significant strides by forming partnerships with the Against Malaria Foundation and the Rainforest Trust to combat malaria and climate change respectively, as well as receiving a $10,000 a month ad grant from Google! Once again, this has all been possible thanks to you and we urge you to please share OpenTabs with your friends and family to further grow our mission of reducing poverty and fighting climate change.
+          <br />
+          <br />
+          New Features Include:
+          <ul>
+            <li>A new and improved design</li>
+            <li>Daily-changing stunning wallpapers</li>
+            <li>A secure and privacy-centred account system.</li>
+          </ul>
+          If you haven't yet, we invite you to create an account (by clicking on the settings icon on the lower left) to ensure that you never lose your tabs count again, and can sync it across browsers and devices. Stay tuned for exciting updates coming soon.
+          </Toast.Body>
         </Toast>
       </Col>
       <Col xs={3}>
@@ -42,7 +53,7 @@ export default class Header extends Component {
     this.state = { showPopup: false };  
     }  
     
-      togglePopup() {  
+    togglePopup() {  
     this.setState({  
          showPopup: !this.state.showPopup  
     });  
@@ -66,12 +77,13 @@ export default class Header extends Component {
             
               <div className="text-left bottom-left">
               <Dropdown id="settings">
-                <Dropdown.Toggle>
+                <Dropdown.Toggle style={{padding: '0'}}>
                 <FiSettings/>
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
                   <Dropdown.Item eventKey="1" target="_blank" href="http://opentabs.org"><FiHome /> Home</Dropdown.Item>
                   <Dropdown.Item eventKey="2" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScNIVjuhLCUF_CczUf2eCP3VOIiIfl-UhJAsh-f-SJbUq7WnQ/viewform"><FiUser /> Feedback</Dropdown.Item>
+                  <Dropdown.Item eventKey="3" target="_blank" href="https://donorbox.org/opentabs"><FiGift /> Donate</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
               </div>     
