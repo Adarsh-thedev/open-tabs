dce5f52c0478a24a68f1343df3cdbfb6cbf66b4d
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-05-31 00:06:04 -0400

    Add files via upload

--- bhavik/src/components/Searchbar.js
+++ bhavik/src/components/Searchbar.js
@@ -12,7 +12,7 @@ export default class Searchbar extends Component {
                       {/* <input placeholder="&#128269;" type="text" id="q" name="q" title="Search" alt="Search Text" maxlength="256" /> */}
                       <input type="text" id="q" name="q" title="Search" alt="Search Text" maxlength="256" />
                       <input type="hidden" id="cx" name="cx" value="008051653802709586379:hdkkje5v5c1" />
-                      <FiSearch />
+                      <button type="submit" id="searchsubmit"><FiSearch /></button>
                       {/* <input type="image" id="searchSubmit" name="submit" src={google} width="20" height="20" alt="Go" title="Submit Search Query" /> */}
                   </div>
               </form>
