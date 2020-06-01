82451e405ea8a7fba5f7456d17134d6d492ffbb5
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-06-01 11:47:21 -0400

    Update App.js

--- bhavik/src/App.js
+++ bhavik/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
-import Landingpage from './components/trial2allinone';
+import Landingpage from './components/trial4bg';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
