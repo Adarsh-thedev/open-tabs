04ffc304131b1080b319eb6b7a3fe29acbd008b3
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-06-12 10:57:31 -0400

    Update App.js

--- bhavik/src/App.js
+++ bhavik/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
-import Landingpage from './components/LandingPage.js';
+import Landingpage from './components/Landingpage.js';
 import TestLandingpage from './components/testlp.js';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
