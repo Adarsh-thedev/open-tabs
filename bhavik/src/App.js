3727f96dfde7806a2ee84853cc1940841eb765d6
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-06-13 12:53:22 -0400

    Update App.js

--- bhavik/src/App.js
+++ bhavik/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css';
 import Landingpage from './components/Landingpage.js';
-import TestLandingpage from './components/testlp.js';
+// import TestLandingpage from './components/testlp.js';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
@@ -17,9 +17,9 @@ class App extends Component {
           <Route path="/tabbing">
             <Landingpage />
           </Route>
-          <Route path="/test">
-            <TestLandingpage />
-          </Route>
+//           <Route path="/test">
+//             <TestLandingpage />
+//           </Route>
         </Switch>
       </Router>
       // <div className="App">
